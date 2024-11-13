ec3ef27f28f4e3d8ec32b4350410b2b52f2ff787
lucile_utilisateur <lucile.z@hotmail.fr>
2024-11-13 16:50:29 +0100

    keyup + localstorage

--- vite/exo/keyUp.js
+++ vite/exo/keyUp.js
@@ -12,16 +12,26 @@ addEventListener("keyup", (event) => {
 /*
 *  on récupère le bouton du html
 * on écoute le clavier
-* si le mot tapé dans l'input est plus long que 5 caractère
+* si le mot tapé dans l'input est plus long que 5 caractères
 * alors on désactive le bouton
 * pour  buton.disabled = true on aurait pu faire: 
 * (buton.setAttribute('disabled', 'true');
 * et on retranscrit dans un p le contenu de l'input
 */
-
 let buton = document.querySelector('button');
 addEventListener("keyup", (event) => {
     if(input.value.length > 5){
         buton.disabled = true;
     }
+});
+
+/*
+* Enregistrer en locaStorage la value de l'input 
+* et réstituer dans un p 
+*/
+let p = document.getElementsByTagName('p');
+addEventListener("keyup", (event) => {
+    let aVenir = localStorage.getItem('clef');
+    localStorage.setItem('clef', input.value);
+    p[0].innerText = aVenir;
 });
