98f5a4b24f6de614ee5278a35b002f59efe1bed8
lucile_utilisateur <lucile.z@hotmail.fr>
2024-11-13 14:59:17 +0100

    scroll bar avancement

--- vite/exo/toggle.js
+++ vite/exo/toggle.js
@@ -11,15 +11,36 @@ liens[2].addEventListener('click', (e)=>{
 })
 
 document.addEventListener('click',(eventClicke)=>{
-    console.log(eventClicke);
+/*     console.log(eventClicke);
     console.log(eventClicke.clientX);
-    console.log(eventClicke.clientY);
+    console.log(eventClicke.clientY); */
     let img = document.createElement('img');
     img.setAttribute('src', 'https://picsum.photos/200');
     document.body.append(img);
     img.style.position='absolute';
     img.style.top = eventClicke.clientY - img.height/2 +"px";
-    img.style.left = eventClicke.clientX - img.width/2 + "px";
-    console.log(img.style);
-    
+    img.style.left = eventClicke.clientX - img.width/2 + "px";    
+})
+
+
+let vanta = document.body.getElementsByClassName('vanta');
+VANTA.CLOUDS({
+    el: vanta[0],
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
+})
+
+let bar = document.getElementsByClassName('bar');
+document.addEventListener('scroll', (e)=>{
+    console.log(`${document.body.scrollHeight}
+        ${window.innerHeight}
+        ${window.scrollY}`);
+    let scrollMax = document.body.scrollHeight - window.innerHeight;
+    console.log('Scroll MAx',scrollMax);
+    let onEstOu = (scrollY/scrollMax) * 100;
+    console.log(onEstOu);
+    bar[0].style.width= onEstOu + '%';
 })
