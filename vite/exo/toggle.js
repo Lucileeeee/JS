7a8a05ab069ad77ff9aa744331b7887996d54c1a
lucile_utilisateur <lucile.z@hotmail.fr>
2024-11-13 11:41:23 +0100

    creer une image dans le dom

--- vite/exo/toggle.js
+++ vite/exo/toggle.js
@@ -10,3 +10,16 @@ liens[2].addEventListener('click', (e)=>{
     liens[2].classList.toggle('toggleClass');
 })
 
+document.addEventListener('click',(eventClicke)=>{
+    console.log(eventClicke);
+    console.log(eventClicke.clientX);
+    console.log(eventClicke.clientY);
+    let img = document.createElement('img');
+    img.setAttribute('src', 'https://picsum.photos/200');
+    document.body.append(img);
+    img.style.position='absolute';
+    img.style.top = eventClicke.clientY - img.height/2 +"px";
+    img.style.left = eventClicke.clientX - img.width/2 + "px";
+    console.log(img.style);
+    
+})
