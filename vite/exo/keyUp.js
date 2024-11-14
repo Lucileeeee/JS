04d6133d5157354e2925cfcd5d5aee6eddc6fec3
lucile_utilisateur <lucile.z@hotmail.fr>
2024-11-14 11:02:44 +0100

    ajout de la correction de jeff

--- vite/exo/keyUp.js
+++ vite/exo/keyUp.js
@@ -47,5 +47,27 @@ addEventListener("keyup", (event) => {
         restituerDansP(); 
     }
 });
+ 
 
+//! correction de Jeff avec Marked et DomPurify:
+//!(le html n'est pas en cohérence, c'est juste pour l'exemple)
+const zoneTxt = document.querySelector("textarea");
+const zoneRender = document.querySelector("#formRender");
+// Dès qu'on arrive sur la page,
+//On va pré remplir le textarea avec ce que l'on récupère dans le localStorage
+zoneTxt.value = localStorage.getItem('monSuperTexte');
+//Si zoneTxt.value est définit alors on rempli la Div avec ce qu'on récupère dans le local storage
+if(zoneTxt.value){
+//    zoneRender.innerText = localStorage.getItem('monSuperTexte');
+   zoneRender.innerHTML = marked(localStorage.getItem('monSuperTexte'));
+};
+//On détecte ce que tape l'utilisateur dans le textarea
+zoneTxt.addEventListener("keyup", function() {
+//On enregistre ce que tape l'utilisateur dans le localStorage sous le nom "monSuperTexte"
+localStorage.setItem('monSuperTexte',zoneTxt.value);
+//On affiche ce que tape l'utilisateur traduit en marked dans la div     
+//    zoneRender.innerHTML =marked(zoneTxt.value);
+   zoneRender.innerHTML =DOMPurify.sanitize(marked(zoneTxt.value));
+//    zoneRender.innerHTML =marked(zoneTxt.value);
+});
 
