82fd7b0e4c64619df12963c0d61952a42edc6b42
lucile_utilisateur <lucile.z@hotmail.fr>
2024-11-26 11:20:26 +0100

    avec didascalie

--- vite/exo/regex.js
+++ vite/exo/regex.js
@@ -1,24 +1,27 @@
+//? récupérer les input de mail et password:
 let mail = document.querySelector('input[type="email"]');
 let password = document.querySelector('input[type="password"]');
-let consigne = document.getElementById('consigne');
+
+//? définir les regex
 const regex = /^[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$/; 
 const charDecimal = /\d/;
 const charSpecial = /[$&@!]/;
 
-
-mail.addEventListener("keyup", (event) => {
-    if(regex.test(mail.value)== true){
-        mail.style.color='green';
-    } else if (regex.test(mail.value)== false){
-        mail.style.color='red';
-    }
-});
-
-
+//? récupérer la liste de consignes:
 let tropCourt = document.getElementById('tropCourt');
 let pasNumber = document.getElementById('pasNumber');
 let pasSpecial = document.getElementById('pasSpecial');
 
+//? regex du mail:
+mail.addEventListener("keyup", (event) => {
+    if(regex.test(mail.value)){
+        mail.style.color='green';
+    } else if (!regex.test(mail.value)){
+        mail.style.color='red';
+    }
+});
+
+//? regex du password pour vérifier la longueur, qu'il contient un chiffre et un caractère spéciale
 password.addEventListener("keyup", (event) => {
     if (password.value.length <= 6){
         tropCourt.innerText='Le Mot De Passe est trop court';
