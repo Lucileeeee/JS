e050ca3694176ac6aed6724a83a16c5e6463a7a3
Lucileeeee <148850401+Lucileeeee@users.noreply.github.com>
2025-01-02 16:53:09 +0100

    Update firebase.js

--- vite/exo/firebase.js
+++ vite/exo/firebase.js
@@ -1,18 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-console.log(`chachat d'amour`);
 
 
 
